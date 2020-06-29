078e99c1d09ae1ed0270d811efb0813d0c151fd6
leedjwls <leedjwls@naver.com>
2020-06-30 01:11:29 +0900

    Launch Screen

---
+++ Semo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Semo
+//
+//  Created by 이어진 on 2020/06/30.
+//  Copyright © 2020 Eo jin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
